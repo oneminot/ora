7f0065e54f063d168dbdf82b2b51f8c11c9b8059
kushal <kushaldeveloper@gmail.com>
2015-04-15 20:38:25 -0500

    split constraint to a new line

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,6 +1,6 @@
-CREATE TABLE Departments
+create table Departments
   (
-    ID NUMBER(5) PRIMARY KEY,
-    DepartmentIdentifier NVARCHAR2(5) NOT NULL,
-    CONSTRAINT DepartmentIdentifer_Unique UNIQUE (DepartmentIdentifier)
+    id number(5) primary key,
+    DepartmentIdentifier nvarchar2(5),
+    constraint DepartmentIdentifer_Unique unique (DepartmentIdentifier)
   );
