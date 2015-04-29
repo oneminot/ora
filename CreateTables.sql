e4693c790155bf43872af0ec6e04fbaf5f9e7bff
kushal <kushaldeveloper@gmail.com>
2015-04-29 17:23:08 -0500

    drop and create

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,21 +1,16 @@
-begin
-execute immediate 'create table if not exists departments
+drop table courses;
+drop table departments;
+
+create table departments
 (
   DepartmentID number(5) primary key,
   DepartmentIdentifier nvarchar2(5) not null,
   DepartmentName nvarchar2(40) not null,
   constraint DepartmentIdentifer_Unique 
     unique (DepartmentIdentifier)
-)';
-exception 
-  when others then 
-    if sqlcode != 955 then 
-      raise;
-    end if;
-  end;
+);
   
-begin
-execute immediate 'create table if not exists courses
+create table courses
 (
   ID number(5) primary key,
   DepartmentID number(5) not null,
@@ -24,11 +19,4 @@ execute immediate 'create table if not exists courses
   CourseDescription nvarchar2(2000) not null,
   constraint CourseNumber_Unique 
     unique (DepartmentID, CourseNumber)
-)';
-exception 
-  when others then 
-    if sqlcode != 955 then 
-      raise;
-    end if;
-  end;
-  
+);
