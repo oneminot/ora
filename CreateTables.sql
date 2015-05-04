726c268893caf6506ebe1ebda6c48dd65778b135
kushal <kushaldeveloper@gmail.com>
2015-05-04 08:34:35 -0500

    create table classes

--- CreateTables.sql
+++ CreateTables.sql
@@ -12,11 +12,20 @@ CREATE TABLE "departments"
 
 CREATE TABLE "courses"
 (
-  ID                NUMBER(5) PRIMARY KEY,
+  CourseID NUMBER(5) PRIMARY KEY,
   DepartmentID      NUMBER(5)       NOT NULL,
   CourseNumber      NUMBER(5)       NOT NULL,
   CourseName        NVARCHAR2(40)   NOT NULL,
   CourseDescription NVARCHAR2(2000) NOT NULL,
   CONSTRAINT CourseNumber_Unique
   UNIQUE (DepartmentID, CourseNumber)
+);
+
+CREATE TABLE "classes"
+(
+  ClassID     NUMBER(5) PRIMARY KEY,
+  ClassNumber VARCHAR2(6) NOT NULL,
+  CourseID    NUMBER(5)   NOT NULL,
+  CONSTRAINT ClassNumber_Unique
+  UNIQUE (ClassNumber, CourseID)
 );
