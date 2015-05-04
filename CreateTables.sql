a6680c9ca7e9b16fdb2cddbebe481730186b8e3b
kushal <kushaldeveloper@gmail.com>
2015-05-04 09:30:40 -0500

    create table events

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,5 +1,5 @@
-DROP TABLE "courses";
-DROP TABLE "departments";
+-- DROP TABLE "courses";
+-- DROP TABLE "departments";
 
 CREATE TABLE "departments"
 (
@@ -18,7 +18,10 @@ CREATE TABLE "courses"
   CourseName        NVARCHAR2(40)   NOT NULL,
   CourseDescription NVARCHAR2(2000) NOT NULL,
   CONSTRAINT CourseNumber_Unique
-  UNIQUE (DepartmentID, CourseNumber)
+  UNIQUE (DepartmentID, CourseNumber),
+  CONSTRAINT DepartmentID_FK
+  FOREIGN KEY (DepartmentID)
+  REFERENCES "departments" (DepartmentID)
 );
 
 CREATE TABLE "classes"
@@ -27,5 +30,30 @@ CREATE TABLE "classes"
   ClassNumber VARCHAR2(6) NOT NULL,
   CourseID    NUMBER(5)   NOT NULL,
   CONSTRAINT ClassNumber_Unique
-  UNIQUE (ClassNumber, CourseID)
+  UNIQUE (ClassNumber, CourseID),
+  CONSTRAINT CourseID_FK
+  FOREIGN KEY (CourseID)
+  REFERENCES "courses" (CourseID)
+);
+
+CREATE TABLE "events"
+(
+  EventID     NUMBER(5) PRIMARY KEY,
+  ClassID     NUMBER(5) NOT NULL,
+  StartDate   DATE      NOT NULL,
+  EndDate     DATE      NOT NULL,
+  StartTime   DATE      NOT NULL,
+  EndTime     DATE      NOT NULL,
+  IsMonday    NUMBER(1) NOT NULL,
+  IsTuesday   NUMBER(1) NOT NULL,
+  IsWednesday NUMBER(1) NOT NULL,
+  IsThursday  NUMBER(1) NOT NULL,
+  IsFriday    NUMBER(1) NOT NULL,
+  IsSaturday  NUMBER(1) NOT NULL,
+  IsSunday    NUMBER(1) NOT NULL,
+  CONSTRAINT StartDate_Unique
+  UNIQUE (StartDate, EndDate, ClassID),
+  CONSTRAINT ClassID_FK
+  FOREIGN KEY (ClassID)
+  REFERENCES "classes" (ClassID)
 );
