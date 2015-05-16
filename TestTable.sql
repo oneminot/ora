9ff3edb46f1f68e97d483e087aa663582a0eed76
kushal <kushaldeveloper@gmail.com>
2015-05-16 12:47:07 -0500

    add a test sequence and a test trigger

---
+++ TestTable.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "kushaltesttable"
+(
+  KushalTestTableText NVARCHAR2(50) UNIQUE NOT NULL
+  --   CONSTRAINT KushalTestTableID_Unique
+  --   UNIQUE (KushalTestTableID)
+);
+
+CREATE TABLE "kushaltestperson" (
+  c1 NUMBER PRIMARY KEY,
+  c2 NVARCHAR2(10)
+);
+
+CREATE SEQUENCE "kushaltestperson_sequence"
+START WITH 1
+INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER "kushaltestperson_sequence_trigger"
+BEFORE INSERT
+ON "kushaltestperson"
+REFERENCING NEW AS NEW
+FOR EACH ROW
+  BEGIN
+    SELECT "kushaltestperson_sequence".nextval
+    INTO :NEW.c1
+    FROM DUAL;
+  END;
