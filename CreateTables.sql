f26e3fc5ea70413394fd66dee833f01bcea8d8e0
kushal <kushaldeveloper@gmail.com>
2015-05-17 11:28:47 -0500

    add primary key as a constraint for all tables

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,18 +1,3 @@
--- ALTER TABLE KHADA."events" DROP CONSTRAINT CLASSES_EVENTS_FK;
--- ALTER TABLE KHADA."authorarticles" DROP CONSTRAINT AUTHORS_AUTHORARTICLES_FK;
--- ALTER TABLE KHADA."courses" DROP CONSTRAINT DEPARTMENTS_COURSES_FK;
--- ALTER TABLE KHADA."classes" DROP CONSTRAINT COURSES_CLASSES_FK;
--- DROP TABLE KHADA."events";
--- DROP TABLE KHADA."authorarticles";
--- DROP TABLE KHADA."courses";
--- DROP TABLE KHADA."classes";
--- DROP TABLE KHADA."departments";
--- DROP TABLE KHADA."authors";
--- DROP TABLE KHADA."publications";
--- DROP TABLE KHADA."assignments";
--- DROP TABLE KHADA."articles";
--- DROP TABLE KHADA."administrationusers";
-
 CREATE TABLE "departments"
 (
   DepartmentID NUMBER(5),
@@ -27,24 +12,14 @@ CREATE SEQUENCE "departments_sequence"
 START WITH 1
 INCREMENT BY 1;
 
--- CREATE OR REPLACE TRIGGER "departments_sequence_trigger"
--- BEFORE INSERT
--- ON "departments"
--- REFERENCING NEW AS NEW
--- FOR EACH ROW
---   BEGIN
---     SELECT "departments_sequence".nextval
---     INTO :NEW.DepartmentID
---     FROM DUAL;
---   END;
-
 CREATE TABLE "courses"
 (
-  CourseID     NUMBER(5) PRIMARY KEY,
+  CourseID NUMBER(5),
   DepartmentID      NUMBER(5)       NOT NULL,
   CourseNumber NVARCHAR2(6) NOT NULL,
   CourseName        NVARCHAR2(40)   NOT NULL,
   CourseDescription NVARCHAR2(2000) NOT NULL,
+  CONSTRAINT courseid_pk PRIMARY KEY (CourseID),
   CONSTRAINT coursenumber_unique
   UNIQUE (DepartmentID, CourseNumber),
   CONSTRAINT departments_courses_fk
@@ -54,9 +29,10 @@ CREATE TABLE "courses"
 
 CREATE TABLE "classes"
 (
-  ClassID     NUMBER(5) PRIMARY KEY,
+  ClassID NUMBER(5),
   ClassNumber NVARCHAR2(6) NOT NULL,
   CourseID    NUMBER(5)    NOT NULL,
+  CONSTRAINT classid_pk PRIMARY KEY (ClassID),
   CONSTRAINT classnumber_unique
   UNIQUE (ClassNumber, CourseID),
   CONSTRAINT courses_classes_fk
@@ -66,7 +42,7 @@ CREATE TABLE "classes"
 
 CREATE TABLE "events"
 (
-  EventID     NUMBER(5) PRIMARY KEY,
+  EventID NUMBER(5),
   ClassID     NUMBER(5) NOT NULL,
   StartDate   DATE      NOT NULL,
   EndDate     DATE      NOT NULL,
@@ -79,6 +55,7 @@ CREATE TABLE "events"
   IsFriday    CHAR(1) NOT NULL,
   IsSaturday  CHAR(1) NOT NULL,
   IsSunday    CHAR(1) NOT NULL,
+  CONSTRAINT eventid_pk PRIMARY KEY (EventID),
   CONSTRAINT startdate_unique
   UNIQUE (StartDate, EndDate, ClassID),
   CONSTRAINT classes_events_fk
@@ -88,7 +65,7 @@ CREATE TABLE "events"
 
 CREATE TABLE "assignments"
 (
-  AssignmentID               NUMBER(5) PRIMARY KEY,
+  AssignmentID       NUMBER(5),
   CourseID                   NUMBER(5)     NOT NULL,
   AssignmentNumber NVARCHAR2(5) NOT NULL,
   AssignmentName             NVARCHAR2(40) NOT NULL,
@@ -96,33 +73,37 @@ CREATE TABLE "assignments"
   AssignmentHeader           NVARCHAR2(40) NOT NULL,
   AssignmentChapterReference NVARCHAR2(40) NOT NULL,
   AssignmentDetail           NCLOB         NOT NULL,
-  AssignmentIsActive         CHAR(1)       NOT NULL
+  AssignmentIsActive CHAR(1) NOT NULL,
+  CONSTRAINT assignmentid_pk PRIMARY KEY (AssignmentID)
 );
 
 CREATE TABLE "articles" (
-  ArticleID            NUMBER(5) PRIMARY KEY,
+  ArticleID NUMBER(5),
   ArticleTitle         NVARCHAR2(80) NOT NULL,
   RelativeFileLocation NVARCHAR2(80) NOT NULL,
   PublicationID        NUMBER(5)     NOT NULL,
   PageReference        NVARCHAR2(40),
+  CONSTRAINT articleid_pk PRIMARY KEY (ArticleID),
   CONSTRAINT articles_unique
   UNIQUE (ArticleTitle, RelativeFileLocation, PublicationID, PageReference)
 );
 
 CREATE TABLE "authors"
 (
-  AuthorID   NUMBER(5) PRIMARY KEY,
+  AuthorID NUMBER(5),
   AuthorName NVARCHAR2(40) NOT NULL,
-  CONSTRAINT authorarticles_unique
+  CONSTRAINT authorid_pk PRIMARY KEY (AuthorID),
+  CONSTRAINT authorname_unique
   UNIQUE (AuthorName)
 );
 
 CREATE TABLE "publications"
 (
-  PublicationID       NUMBER(5) PRIMARY KEY,
+  PublicationID   NUMBER(5),
   PublicationTitle    NVARCHAR2(80) NOT NULL,
   PublicationLocation NVARCHAR2(80) NOT NULL,
-  PublicationYear     VARCHAR2(4)
+  PublicationYear VARCHAR2(4),
+  CONSTRAINT publicationid_pk PRIMARY KEY (PublicationID)
 );
 
 CREATE TABLE "authorarticles"
@@ -130,7 +111,8 @@ CREATE TABLE "authorarticles"
   AuthorArticleID NUMBER(5) PRIMARY KEY,
   AuthorID        NUMBER(5) NOT NULL,
   ArticleID       NUMBER(5) NOT NULL,
-  CONSTRAINT authorid_articleid_unique
+  CONSTRAINT authorarticleid_pk PRIMARY KEY (AuthorArticleID),
+  CONSTRAINT authorarticles_unique
   UNIQUE (AuthorID, ArticleID),
   CONSTRAINT authors_authorarticles_fk
   FOREIGN KEY (AuthorID)
@@ -138,10 +120,11 @@ CREATE TABLE "authorarticles"
 );
 
 CREATE TABLE "administrationusers" (
-  AdministrationUserID           NUMBER(5) PRIMARY KEY NOT NULL,
+  AdministrationUserID          NUMBER(5) NOT NULL,
   AdministrationUserName         NVARCHAR2(255)        NOT NULL,
   AdministrationUserSalt         NVARCHAR2(2000)       NOT NULL,
   AdministrationUserPassword     NVARCHAR2(2000)       NOT NULL,
   AdministrationUserAttemptCount NUMBER(5)             NOT NULL,
-  AdministrationUserLastAttempt  TIMESTAMP             NOT NULL
+  AdministrationUserLastAttempt TIMESTAMP NOT NULL,
+  CONSTRAINT administrationuserid_pk PRIMARY KEY (AdministrationUserID)
 );
