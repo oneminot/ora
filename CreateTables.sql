792b6f80f94dae79ecae9af3e5e1114800eeb35e
kushal <kushaldeveloper@gmail.com>
2015-05-17 12:34:47 -0500

    replace all nvarchar2 and put varchar2 instead
    the project requirements will not need unicode

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "departments"
 (
-  DepartmentID NUMBER(5),
-  DepartmentIdentifier NVARCHAR2(5)  NOT NULL,
-  DepartmentName       NVARCHAR2(40) NOT NULL,
+  DepartmentID         NUMBER(5),
+  DepartmentIdentifier VARCHAR2(5)  NOT NULL,
+  DepartmentName       VARCHAR2(40) NOT NULL,
   CONSTRAINT departmentid_pk PRIMARY KEY (DepartmentID),
   CONSTRAINT departmentidentifier_unique
   UNIQUE (DepartmentIdentifier)
@@ -14,11 +14,11 @@ INCREMENT BY 1;
 
 CREATE TABLE "courses"
 (
-  CourseID     NUMBER(5),
-  DepartmentID      NUMBER(5)       NOT NULL,
-  CourseNumber NVARCHAR2(6) NOT NULL,
-  CourseName        NVARCHAR2(40)   NOT NULL,
-  CourseDescription NVARCHAR2(2000) NOT NULL,
+  CourseID          NUMBER(5),
+  DepartmentID      NUMBER(5)      NOT NULL,
+  CourseNumber      VARCHAR2(6)    NOT NULL,
+  CourseName        VARCHAR2(40)   NOT NULL,
+  CourseDescription VARCHAR2(2000) NOT NULL,
   CONSTRAINT courseid_pk PRIMARY KEY (CourseID),
   CONSTRAINT coursenumber_unique
   UNIQUE (DepartmentID, CourseNumber),
@@ -29,9 +29,9 @@ CREATE TABLE "courses"
 
 CREATE TABLE "classes"
 (
-  ClassID NUMBER(5),
-  ClassNumber NVARCHAR2(6) NOT NULL,
-  CourseID    NUMBER(5)    NOT NULL,
+  ClassID     NUMBER(5),
+  ClassNumber VARCHAR2(6) NOT NULL,
+  CourseID    NUMBER(5)   NOT NULL,
   CONSTRAINT classid_pk PRIMARY KEY (ClassID),
   CONSTRAINT classnumber_unique
   UNIQUE (ClassNumber, CourseID),
@@ -65,37 +65,37 @@ CREATE TABLE "events"
 
 CREATE TABLE "assignments"
 (
-  AssignmentID       NUMBER(5),
-  CourseID                   NUMBER(5)     NOT NULL,
-  AssignmentNumber   NVARCHAR2(5) NOT NULL,
-  AssignmentName             NVARCHAR2(40) NOT NULL,
-  AssignmentDueDate          DATE          NOT NULL,
-  AssignmentHeader           NVARCHAR2(40) NOT NULL,
-  AssignmentChapterReference NVARCHAR2(40) NOT NULL,
-  AssignmentDetail           NCLOB         NOT NULL,
-  AssignmentIsActive CHAR(1)      NOT NULL,
+  AssignmentID               NUMBER(5),
+  CourseID                   NUMBER(5)    NOT NULL,
+  AssignmentNumber           VARCHAR2(5)  NOT NULL,
+  AssignmentName             VARCHAR2(40) NOT NULL,
+  AssignmentDueDate          DATE         NOT NULL,
+  AssignmentHeader           VARCHAR2(40) NOT NULL,
+  AssignmentChapterReference VARCHAR2(40) NOT NULL,
+  AssignmentDetail           NCLOB        NOT NULL,
+  AssignmentIsActive         CHAR(1)      NOT NULL,
   CONSTRAINT assignmentid_pk PRIMARY KEY (AssignmentID)
 );
 
 CREATE TABLE "syllabi"
 (
   SyllabusID                 NUMBER(5),
-  CourseID                   NUMBER(5)     NOT NULL,
-  AssignmentName             NVARCHAR2(40) NOT NULL,
-  AssignmentDueDate          DATE          NOT NULL,
-  AssignmentHeader           NVARCHAR2(40) NOT NULL,
-  AssignmentChapterReference NVARCHAR2(40) NOT NULL,
-  AssignmentDetail           NCLOB         NOT NULL,
-  SyllabusIsActive           CHAR(1)       NOT NULL,
+  CourseID                   NUMBER(5)    NOT NULL,
+  AssignmentName             VARCHAR2(40) NOT NULL,
+  AssignmentDueDate          DATE         NOT NULL,
+  AssignmentHeader           VARCHAR2(40) NOT NULL,
+  AssignmentChapterReference VARCHAR2(40) NOT NULL,
+  AssignmentDetail           NCLOB        NOT NULL,
+  SyllabusIsActive           CHAR(1)      NOT NULL,
   CONSTRAINT syllabusid_pk PRIMARY KEY (SyllabusID)
 );
 
 CREATE TABLE "articles" (
-  ArticleID NUMBER(5),
-  ArticleTitle         NVARCHAR2(80) NOT NULL,
-  RelativeFileLocation NVARCHAR2(80) NOT NULL,
-  PublicationID        NUMBER(5)     NOT NULL,
-  PageReference        NVARCHAR2(40),
+  ArticleID            NUMBER(5),
+  ArticleTitle         VARCHAR2(80) NOT NULL,
+  RelativeFileLocation VARCHAR2(80) NOT NULL,
+  PublicationID        NUMBER(5)    NOT NULL,
+  PageReference        VARCHAR2(40),
   CONSTRAINT articleid_pk PRIMARY KEY (ArticleID),
   CONSTRAINT articles_unique
   UNIQUE (ArticleTitle, RelativeFileLocation, PublicationID, PageReference)
@@ -103,8 +103,8 @@ CREATE TABLE "articles" (
 
 CREATE TABLE "authors"
 (
-  AuthorID NUMBER(5),
-  AuthorName NVARCHAR2(40) NOT NULL,
+  AuthorID   NUMBER(5),
+  AuthorName VARCHAR2(40) NOT NULL,
   CONSTRAINT authorid_pk PRIMARY KEY (AuthorID),
   CONSTRAINT authorname_unique
   UNIQUE (AuthorName)
@@ -112,10 +112,10 @@ CREATE TABLE "authors"
 
 CREATE TABLE "publications"
 (
-  PublicationID   NUMBER(5),
-  PublicationTitle    NVARCHAR2(80) NOT NULL,
-  PublicationLocation NVARCHAR2(80) NOT NULL,
-  PublicationYear VARCHAR2(4),
+  PublicationID       NUMBER(5),
+  PublicationTitle    VARCHAR2(80) NOT NULL,
+  PublicationLocation VARCHAR2(80) NOT NULL,
+  PublicationYear     VARCHAR2(4),
   CONSTRAINT publicationid_pk PRIMARY KEY (PublicationID)
 );
 
@@ -133,11 +133,11 @@ CREATE TABLE "authorarticles"
 );
 
 CREATE TABLE "administrationusers" (
-  AdministrationUserID           NUMBER(5)       NOT NULL,
-  AdministrationUserName         NVARCHAR2(255)  NOT NULL,
-  AdministrationUserSalt         NVARCHAR2(2000) NOT NULL,
-  AdministrationUserPassword     NVARCHAR2(2000) NOT NULL,
-  AdministrationUserAttemptCount NUMBER(5)       NOT NULL,
-  AdministrationUserLastAttempt  TIMESTAMP       NOT NULL,
+  AdministrationUserID           NUMBER(5)      NOT NULL,
+  AdministrationUserName         VARCHAR2(255)  NOT NULL,
+  AdministrationUserSalt         VARCHAR2(2000) NOT NULL,
+  AdministrationUserPassword     VARCHAR2(2000) NOT NULL,
+  AdministrationUserAttemptCount NUMBER(5)      NOT NULL,
+  AdministrationUserLastAttempt  TIMESTAMP      NOT NULL,
   CONSTRAINT administrationuserid_pk PRIMARY KEY (AdministrationUserID)
 );
