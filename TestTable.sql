c24217ba09d6c0c0ab0cdb95c1551732d4357560
kushal <kushaldeveloper@gmail.com>
2015-05-18 11:31:44 -0500

    add nocache

--- TestTable.sql
+++ TestTable.sql
@@ -1,30 +1,31 @@
-ALTER TABLE "testpersons" DROP CONSTRAINT pk_testpersons;
-DROP SEQUENCE "testperson_sequence";
-DROP TABLE "testpersons";
+-- ALTER TABLE "testpersons" DROP CONSTRAINT pk_testpersons;
+-- DROP SEQUENCE KHADA.test_person_sequence;
+-- DROP TABLE KHADA.test_persons;
 
-CREATE TABLE "testpersons" (
-  ID           NUMBER(5)      NOT NULL,
-  Name         VARCHAR2(255)  NOT NULL,
-  Password     VARCHAR2(2000) NOT NULL,
-  AttemptCount NUMBER(5)      NOT NULL,
-  LastAttempt  TIMESTAMP      NOT NULL,
-  CONSTRAINT pk_testpersons PRIMARY KEY (ID)
-);
-
-CREATE SEQUENCE "testperson_sequence"
-START WITH 1
-INCREMENT BY 1;
-
--- CREATE OR REPLACE TRIGGER "testperson_trigger"
--- BEFORE INSERT
--- ON "testpersons"
--- REFERENCING NEW AS NEW
--- FOR EACH ROW
---   BEGIN
---     SELECT "testperson_sequence".nextval
---     INTO :NEW.ID
---     FROM DUAL;
---   END;
-
-INSERT INTO testpersons (ID, Name, Password, AttemptCount, LastAttempt)
-VALUES ("testperson_sequence".nextval, "Kushal", "aphromoo", 1, SYSDATE);
+-- CREATE TABLE testpersons (
+--   ID           NUMBER(5)      NOT NULL,
+--   Name         VARCHAR2(255)  NOT NULL,
+--   Password     VARCHAR2(2000) NOT NULL,
+--   AttemptCount NUMBER(5)      NOT NULL,
+--   LastAttempt  TIMESTAMP      NOT NULL,
+--   CONSTRAINT pk_testpersons PRIMARY KEY (ID)
+-- );
+--
+-- CREATE SEQUENCE testperson_sequence
+-- START WITH 1
+-- INCREMENT BY 1
+-- NOCACHE ;
+--
+-- -- CREATE OR REPLACE TRIGGER "testperson_trigger"
+-- -- BEFORE INSERT
+-- -- ON "testpersons"
+-- -- REFERENCING NEW AS NEW
+-- -- FOR EACH ROW
+-- --   BEGIN
+-- --     SELECT "testperson_sequence".nextval
+-- --     INTO :NEW.ID
+-- --     FROM DUAL;
+-- --   END;
+--
+-- INSERT INTO testpersons (ID, Name, Password, AttemptCount, LastAttempt)
+-- VALUES (testperson_sequence.nextval, 'Kushal', 'aphromoo', 1, systimestamp);
