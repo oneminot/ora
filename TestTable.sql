650127940ff424765d9cf0e403ba566d1689b59d
kushal <kushaldeveloper@gmail.com>
2015-05-18 10:59:25 -0500

    try a test table

--- TestTable.sql
+++ TestTable.sql
@@ -1,24 +1,30 @@
-ALTER TABLE "testperson" DROP CONSTRAINT testperson_pk;
+ALTER TABLE "testpersons" DROP CONSTRAINT pk_testpersons;
 DROP SEQUENCE "testperson_sequence";
-DROP TABLE "testperson";
+DROP TABLE "testpersons";
 
-CREATE TABLE "testperson" (
-  c1 NUMBER        NOT NULL,
-  c2 NVARCHAR2(10) NOT NULL,
-  CONSTRAINT testperson_pk PRIMARY KEY (c1)
+CREATE TABLE "testpersons" (
+  ID           NUMBER(5)      NOT NULL,
+  Name         VARCHAR2(255)  NOT NULL,
+  Password     VARCHAR2(2000) NOT NULL,
+  AttemptCount NUMBER(5)      NOT NULL,
+  LastAttempt  TIMESTAMP      NOT NULL,
+  CONSTRAINT pk_testpersons PRIMARY KEY (ID)
 );
 
 CREATE SEQUENCE "testperson_sequence"
 START WITH 1
 INCREMENT BY 1;
 
-CREATE OR REPLACE TRIGGER "testperson_trigger"
-BEFORE INSERT
-ON "testperson"
-REFERENCING NEW AS NEW
-FOR EACH ROW
-  BEGIN
-    SELECT "testperson_sequence".nextval
-    INTO :NEW.c1
-    FROM DUAL;
-  END;
+-- CREATE OR REPLACE TRIGGER "testperson_trigger"
+-- BEFORE INSERT
+-- ON "testpersons"
+-- REFERENCING NEW AS NEW
+-- FOR EACH ROW
+--   BEGIN
+--     SELECT "testperson_sequence".nextval
+--     INTO :NEW.ID
+--     FROM DUAL;
+--   END;
+
+INSERT INTO testpersons (ID, Name, Password, AttemptCount, LastAttempt)
+VALUES ("testperson_sequence".nextval, "Kushal", "aphromoo", 1, SYSDATE);
