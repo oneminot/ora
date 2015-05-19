b7d52cc6c50c313e2551a5d40e48bcf21ba32929
kushal <kushaldeveloper@gmail.com>
2015-05-19 09:07:37 -0500

    learn functions in pl sql

--- TestTable.sql
+++ TestTable.sql
@@ -1,31 +1,42 @@
--- ALTER TABLE "testpersons" DROP CONSTRAINT pk_testpersons;
--- DROP SEQUENCE KHADA.test_person_sequence;
+-- ALTER TABLE "test_persons" DROP CONSTRAINT pk_test_persons;
+-- DROP SEQUENCE KHADA.seq_test_persons;
 -- DROP TABLE KHADA.test_persons;
-
--- CREATE TABLE testpersons (
+--
+-- CREATE TABLE test_persons (
 --   ID           NUMBER(5)      NOT NULL,
 --   Name         VARCHAR2(255)  NOT NULL,
 --   Password     VARCHAR2(2000) NOT NULL,
 --   AttemptCount NUMBER(5)      NOT NULL,
 --   LastAttempt  TIMESTAMP      NOT NULL,
---   CONSTRAINT pk_testpersons PRIMARY KEY (ID)
+--   CONSTRAINT pk_test_persons PRIMARY KEY (ID)
 -- );
 --
--- CREATE SEQUENCE testperson_sequence
+-- CREATE SEQUENCE seq_test_persons
 -- START WITH 1
 -- INCREMENT BY 1
 -- NOCACHE ;
 --
--- -- CREATE OR REPLACE TRIGGER "testperson_trigger"
--- -- BEFORE INSERT
--- -- ON "testpersons"
--- -- REFERENCING NEW AS NEW
--- -- FOR EACH ROW
--- --   BEGIN
--- --     SELECT "testperson_sequence".nextval
--- --     INTO :NEW.ID
--- --     FROM DUAL;
--- --   END;
+-- CREATE OR REPLACE TRIGGER trigger_identity_test_persons
+-- BEFORE INSERT
+-- ON test_persons
+-- REFERENCING NEW AS NEW
+-- FOR EACH ROW
+--   BEGIN
+--     SELECT seq_test_persons.nextval
+--     INTO :NEW.ID
+--     FROM DUAL;
+--   END;
+
+-- INSERT INTO test_persons (ID, Name, Password, AttemptCount, LastAttempt)
+-- VALUES (NULL, 'Kushal', 'aphromoo', 1, systimestamp);
+
+SELECT 1
+FROM test_persons
+ORDER BY LastAttempt DESC;
+
+-- create or REPLACE FUNCTION func_test_persons_insert
+--   [Name_In IN varchar2(255), Password_In IN varchar2(2000)]
+--   RETURN number
+--   IS time_since_last_attempt timestamp;
+-- cursor c1 is select 1 from test_persons order by LastAttempt DESC;
 --
--- INSERT INTO testpersons (ID, Name, Password, AttemptCount, LastAttempt)
--- VALUES (testperson_sequence.nextval, 'Kushal', 'aphromoo', 1, systimestamp);
